ebae2c7a79bf3e343b4a5d854698b23c10be390b
ronic009 <77404768+ronic009@users.noreply.github.com>
2025-02-26 16:37:23 -0800

    Update config.js

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
 // MODIFICA ESTA URL POR LA DEL SERVICIO WEB DESPUES DE HABERLO DESPLEGADO
-const AS_URL_BASE = "https://script.google.com/macros/s/AquiVaTuURL/exec";
+const AS_URL_BASE = "https://script.google.com/a/~/macros/s/AKfycbwE2aR7SR8U_YVKIAwhi6g-yyuC-W_CITayzRKSorEKdcutoABL-roD4lupC_DtM2xj/exec";
