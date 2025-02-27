53390c2a4afe4c50587e3bfcca194d42ab0edf12
ronic009 <77404768+ronic009@users.noreply.github.com>
2025-02-26 16:54:57 -0800

    Update config.js

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
 // MODIFICA ESTA URL POR LA DEL SERVICIO WEB DESPUES DE HABERLO DESPLEGADO
-const AS_URL_BASE = "https://script.google.com/a/~/macros/s/AKfycbwE2aR7SR8U_YVKIAwhi6g-yyuC-W_CITayzRKSorEKdcutoABL-roD4lupC_DtM2xj/exec";
+const AS_URL_BASE = "https://script.google.com/a/~/macros/s/AKfycbxpUbvUuS06vkHF0CTXtMCSTGaKoNJ70zcocKr-JDsjqc4oaBO2b9oxeR5_bFWBZWry/exec?action=checkin&control=Entrada";
